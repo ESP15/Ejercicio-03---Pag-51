0738e10ba89d2c0a8aa183f8ce03c073e14c1e99
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-04-03 13:39:46 +0200

    Add files via upload

---
+++ ejerciciostrings/main.js
@@ -0,0 +1,41 @@
+let nombre;
+nombre='Eric'; /*iniciación de un valor*/
+console.log (nombre);
+console.log (typeof(nombre));
+let edad
+edad=29;
+console.log(edad);
+console.log(typeof(edad));
+let booleano=false;
+console.log('false');
+console.log(typeof booleano);
+let dni=null;
+console.log(typeof dni);
+let altura=undefined;
+console.log(typeof altura);
+
+//sirve para demostración de que no son iguales*//
+
+const value1= Symbol("hello");
+const value2= Symbol("hello");
+console.log(value1==value2); //false
+console.log (value1.description==value2.description);//true
+
+//Y demuestra dos resultados//
+
+const valor1=Symbo1('ESP15');
+console.log(value1);
+console.log(typeof value1);
+
+const object={nombre: 'Eric', apellido: 'Sanz Peiri'};
+console.log (object);
+console.log (typeof object);
+
+//Para hacer la tabla//
+
+const object1={nombre: 'Eric', apellido 'Sanz Peiri', dni '39995059Y'};
+console.table(object1);
+console.log(typeof object1);
+
+//Para hacer tabla //
+
